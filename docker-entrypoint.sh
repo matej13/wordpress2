d2bace7fc3d92c225eb4121ea6d87425200ff569
matej13 <matej13@users.noreply.github.com>
2017-06-13 16:53:08 +0200

    Create docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,38 @@
 #!/bin/sh
-set -euo pipefail
 
-# first arg is `-f` or `--some-option`
-if [ "${1#-}" != "$1" ]; then
-	set -- wp "$@"
+set -x
+
+MY_WP_ROOT=/var/www/html
+WP_CONTENT_ROOT=/data/wp-content
+MY_SRV=$(echo $OPENSHIFT_BUILD_NAME|awk -F'-' '{print $1"-"$2}')
+
+WP_SITEURL=https://${MY_SRV}-${OPENSHIFT_BUILD_NAMESPACE}.${ENV_SUB_DOMAIN}
+
+export MY_WP_ROOT WP_CONTENT_ROOT MY_SRV
+
+if [ ! -f ${MY_WP_ROOT}/wp-config.php ]; then
+  MY_SALTS=$(curl -sS https://api.wordpress.org/secret-key/1.1/salt/ )
+  export MY_SALTS
 fi
 
-# if our command is a valid wp-cli subcommand, let's invoke it through wp-cli instead
-# (this allows for "docker run wordpress:cli help", etc)
-if wp --path=/dev/null help "$1" > /dev/null 2>&1; then
-	set -- wp "$@"
+if [ ! -f  ${WP_CONTENT_ROOT}/index.php ]; then
+  mv /tmp/wp-content/* ${WP_CONTENT_ROOT}/
+  rmdir /tmp/wp-content/
+  unzip /tmp/wp-mail-smtp.0.9.6.zip -d ${WP_CONTENT_ROOT}/plugins/
 fi
 
-exec "$@"
+# ENV_SUB_DOMAIN must be set in the template depend on playground or provided
+# COOKIE_DOMAIN wordpress-${OPENSHIFT_BUILD_NAMESPACE}.${ENV_SUB_DOMAIN}
+# WP_SITEURL must be set over template
+# https://wordpress-${OPENSHIFT_BUILD_NAMESPACE}.${ENV_SUB_DOMAIN}
+
+# WP_CONTENT_DIR must point to the pv/pvc
+if [ ! -s ${MY_WP_ROOT}/wp-config.php ]; then
+  # due to the fact that $table_prefix will be substituted
+  # we must handle this corner case with sed
+  envsubst < ${MY_WP_ROOT}/wp-config.php.template | \
+  envsubst | \
+  sed 's/\$ /\$/g' > ${MY_WP_ROOT}/wp-config.php
+fi
+
+exec /opt/rh/httpd24/root/sbin/httpd-scl-wrapper -D FOREGROUND
